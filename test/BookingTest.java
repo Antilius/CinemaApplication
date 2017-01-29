3c675cb01c88c23d8e2e3b52be8d565edcd981b7
Aga <Aga@AgaAsus>
2017-01-30 00:16:26 +0100

    movie test

--- test/BookingTest.java
+++ test/BookingTest.java
@@ -4,8 +4,16 @@
  * and open the template in the editor.
  */
 
+import Memory.HallsForDayInMemory;
+import ObjectsInCinema.BookedPlace;
+import ObjectsInCinema.Booking;
 import ObjectsInCinema.Movie;
 import ObjectsInCinema.MovieType;
+import ObjectsInCinema.OneDayRepertoire;
+import ObjectsInCinema.Showing;
+import People.Person;
+import PersonalizedDates.DateFormatting;
+import java.util.HashMap;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,35 +28,43 @@ import static org.junit.Assert.*;
 public class BookingTest {
     
     public BookingTest() {
-    }
     
-    @BeforeClass
-    public static void setUpClass() {
-        //Movie movie = new Movie("tytul", "rezyser", MovieType.ANIMATED,16,120);
-     
-        
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
     }
 
+     @Before
+    public void setUp() {
+       
+    }
+    
+    
+    //testowac osobno, razem error
     @Test
     public void bookFreePlace(){
+
+        Movie movie = new Movie("tytul", "rezyser", MovieType.ANIMATED,16,120);
+        Person person = new Person("Imie", "Nazwisko", 2,"email","login","password", false);
+        DateFormatting date = new DateFormatting();
+        BookedPlace place = new BookedPlace(1,7);//zmieniac za kazdym razem
+        Showing showing = new Showing (movie,1,"20.00");
+      
+        Booking booking = new Booking(date,person, showing, place);
+        
+       
+        assertTrue( booking.acceptingBooking(booking));
         
     }
     
     @Test 
     public void bookTakenPlace(){
-        
+        Movie movie = new Movie("tytul1", "rezyser1", MovieType.ANIMATED,16,120);
+        Person person = new Person("Imie", "Nazwisko", 2,"email","login","password", false);
+        DateFormatting date = new DateFormatting();
+        Showing showing = new Showing (movie,2,"20.00");
+        BookedPlace place = new BookedPlace(1, 2); //zmieniac za kazdym razem
+        Booking booking = new Booking(date, person,showing, place);
+        Booking booking2 = new Booking(date, person,showing, place);
+        booking.acceptingBooking(booking);
+        assertFalse(booking2.acceptingBooking(booking));
     }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
